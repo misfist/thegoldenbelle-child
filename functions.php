60ea8fc0e2aa298af6981250780177da89930aa8
Pea Lutz <pea@glocal.coop>
2015-12-15 16:54:44 -0800

    Removed boilerplate styles. Tweak home styles.

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+if(! function_exists( 'thegoldenbelle_enqueue_styles' ) ) {
+
+    function thegoldenbelle_enqueue_styles() {
+        $parent_style = 'ebor-style';
+        $icon_fonts = 'ebor-style';
+
+        wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+        wp_enqueue_style( 'thegoldenbelle-style',
+            trailingslashit( get_stylesheet_directory_uri() ) . 'style.css',
+            array( $parent_style )
+        );
+    }
+
+    add_action( 'wp_enqueue_scripts', 'thegoldenbelle_enqueue_styles' );
+
+}
+
+?>
