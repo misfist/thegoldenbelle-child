6b2e1c69a8ccee108cfa826e3bcebd61617efaab
Pea Lutz <pea@glocal.coop>
2016-01-30 02:18:03 -0500

    Added footer override.

---
+++ footer-singlewidget.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+
+	<div class="container inner">
+	  <div class="row">
+	
+		<?php dynamic_sidebar('singlewidget'); ?>
+	    
+	  </div>
+	</div>
+	
+	<div class="sub-footer">
+	  <div class="container">
+	    
+	    <div class="text-center">
+	    	<?php dynamic_sidebar( 'footer-copyright' ); ?>
+	    </div>
+	    
+	  </div>
+	</div>
+
+</footer>
